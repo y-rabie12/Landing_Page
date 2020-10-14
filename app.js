fc59a18c946d4f1a45ea9bf3e15ee2b847ac62c5
y-rabie12 <63198967+y-rabie12@users.noreply.github.com>
2020-10-14 23:36:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,30 +3,48 @@
    project:
    1- Create a dynamic navbar that automatically adds a new link as a new 
       section is  added.
-   2-Add
+   2- Add smooth scrolling behavior to the links. 
+   3- Show which section is in the active state. 
+   
 
 */
-const sections = document.querySelectorAll('.sections');//
-const documentFrag = document.createDocumentFragment();
-const navList = document.getElementById('navbar__list');
-
+// This is the first that involves creating the dynamic navbar and smooth scrolling behavior. 
+const sections = document.querySelectorAll('.sections');// sections stores a Node List of the section on the page. 
+const documentFrag = document.createDocumentFragment(); // This document fragment is used when the list items and links are created. 
+const navList = document.getElementById('navbar__list'); // navlist stores the 'ul' element. 
+/*
+  This for each loop, loops on all the sections of the page creating 
+  an list element and an anchor element for each section found on the page
+*/
  sections.forEach(function(section)
 {
+    //create an 'li' element.
     let list = document.createElement('li');
+    // create an 'a' element. 
     let anchorTag = document.createElement('a');
+
+    // append the list element to the 'ul' and append the 'a' element to the list element. 
     navList.appendChild(list);
     list.appendChild(anchorTag);
+
+    // get the data-nav attribute for each section. 
     let dataNav = section.getAttribute('data-nav');
-    let ids = "#" + section.getAttribute('id');
-    anchorTag.setAttribute('href',ids);
-    anchorTag.innerHTML= dataNav;
+    let ids = "#" + section.getAttribute('id'); // 'ids' stores the ID attribute of each section. 
+    anchorTag.setAttribute('href',ids); // for the value for each 'href' to the 'ids'. 
+    anchorTag.innerHTML= dataNav; // set the content of each anchorTags to the data-nav attribute. 
+
+    // This part adds the smooth scrolling behavior. 
     list.addEventListener('click',function(event)
     {
+        // Here, once clicked, prevent the default behavior which is scrolling so fast. 
         event.preventDefault();
-        section.scrollIntoView({behavior:'smooth'})
+
+        // for each section add the scrollIntoView to create smooth scrolling behavior. 
+        section.scrollIntoView({behavior:'smooth'});
     })
 
 })
+// Finally, append the document fraent to the unordered list. 
 navList.appendChild(documentFrag);
 
 
@@ -51,5 +69,5 @@ function isInviewport(entries,observer)
     })
 }
 
-sections.forEach(section => {observer.observe(section)})
+sections.forEach(section => {observer.observe(section)});
      
